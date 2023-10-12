93e5598e5f82e70150563f620cd88f48079ae37e
Milan Hergott <milan.hergott@gmail.com>
2023-10-12 22:48:29 +0200

    add dependant files jquery and  datatables

--- dicoco-web/src/App.js
+++ dicoco-web/src/App.js
@@ -33,7 +33,31 @@ function App() {
           Learn React
         </a>
       </header>
+      <table id="myTable" class="display">
+        <thead>
+            <tr>
+                <th>Column 1</th>
+                <th>Column 2</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Row 1 Data 1</td>
+                <td>Row 1 Data 2</td>
+            </tr>
+            <tr>
+                <td>Row 2 Data 1</td>
+                <td>Row 2 Data 2</td>
+            </tr>
+        </tbody>
+      </table>
+      <script>
+      let table = new DataTable('#myTable', {
+    // options
+      });
+      </script>
     </div>
+  
   );
 }
 
