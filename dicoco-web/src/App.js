b14dec95aba8be2219550e33fb9ee3490242c1c2
cjezierski <cjezierski@softia.fr>
2024-01-21 17:15:36 +0100

    add autocapitalize none for phone users

--- dicoco-web/src/App.js
+++ dicoco-web/src/App.js
@@ -450,37 +450,55 @@ function App() {
                     <legend>Phonétique:</legend>
                     <div className={"form-child"}>
                         <label>Commence par :</label>
-                        <input {...register('startsWithPhoetically')} />
+                        <input
+                            autoCapitalize="none"
+                            {...register('startsWithPhoetically')}
+                        />
                         <p className={"message-error"}>{errors.startsWithPhoetically?.message}</p>
                     </div>
                     <div className={"form-child"}>
                         <label>Finit par :</label>
-                        <input {...register('endedWithPhoetically')} />
+                        <input autoCapitalize="none"
+                               {...register('endedWithPhoetically')}
+                        />
                         <p className={"message-error"}>{errors.endedWithPhoetically?.message}</p>
                     </div>
                     <div className={"form-child"}>
                         <label>Contient :</label>
-                        <input {...register('containsPhoetically')} />
+                        <input
+                            autoCapitalize="none"
+                            {...register('containsPhoetically')}
+                        />
                         <p className={"message-error"}>{errors.containsPhoetically?.message}</p>
                     </div>
                     <div className={"form-child"}>
                         <label>Contient à la suite :</label>
-                        <input {...register('containsFollowingPhoetically')} />
+                        <input
+                            autoCapitalize="none"
+                               {...register('containsFollowingPhoetically')}
+                        />
                         <p className={"message-error"}>{errors.containsFollowingPhoetically?.message}</p>
                     </div>
                     <div className={"form-child"}>
                         <label>Anagramme :</label>
-                        <input {...register('anagramPhoetically')} />
+                        <input
+                            autoCapitalize="none"
+                            {...register('anagramPhoetically')}
+                        />
                         <p className={"message-error"}>{errors.anagramPhoetically?.message}</p>
                     </div>
                     <div className={"form-child"}>
                         <label>Anagramme moins:</label>
-                        <input {...register('anagramMinusPhoetically')} />
+                        <input autoCapitalize="none"
+                               {...register('anagramMinusPhoetically')}
+                        />
                         <p className={"message-error"}>{errors.anagramMinusPhoetically?.message}</p>
                     </div>
                     <div className={"form-child"}>
                         <label>Anagramme plus:</label>
-                        <input {...register('anagramPlusPhoetically')} />
+                        <input autoCapitalize="none"
+                               {...register('anagramPlusPhoetically')}
+                        />
                         <p className={"message-error"}>{errors.anagramPlusPhoetically?.message}</p>
                     </div>
                 </fieldset>
