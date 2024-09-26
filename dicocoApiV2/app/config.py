3f19023f4a590a797199722dbdd48e359e91e1ac
cjezierski <corentin.jezierski@gmail.com>
2024-09-26 16:57:51 +0200

    remove library flask-mysqldb

--- dicocoApiV2/app/config.py
+++ dicocoApiV2/app/config.py
@@ -13,4 +13,3 @@ class Config:
     SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DB')}"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
